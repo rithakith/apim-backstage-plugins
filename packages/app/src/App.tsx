7a7e86422f7ff670ec55d5c36aadc5583a724086
rithakith <ritharaedirisinghe@gmail.com>
2026-07-28 12:01:33 +0530

    Revert "Rename @wso2 scope to @rithakith for fork testing"
    This reverts commit 40d6cbc141a2b688ec43b2ad745655927358bc07.

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,6 +1,6 @@
 import { createApp } from '@backstage/frontend-defaults';
 import catalogPlugin from '@backstage/plugin-catalog/alpha';
-import wso2ApiPlatformPlugin from '@rithakith/backstage-plugin-wso2-api-platform/alpha';
+import wso2ApiPlatformPlugin from '@wso2/backstage-plugin-wso2-api-platform/alpha';
 import { navModule } from './modules/nav';
 
 export default createApp({
